bed4c18e49400d4b60b17e71cbcccbe7233f08e6
Rusty Brooks <me@rustybrooks.com>
2022-01-07 13:19:17 -0600

    Some improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,45 @@
 const eslint = {
-	extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb-base",
-    "airbnb-typescript/base"
-	],
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint", "react-hooks", 'jest', 'prettier'],
-  "parserOptions": {
-    "project": "./tsconfig.json"
+  'parser': '@typescript-eslint/parser',
+  'plugins': ['@typescript-eslint'],
+  extends: [
+    'plugin:jest/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
+  'parserOptions': {
+    'project': './tsconfig.json'
   },
-  "rules": {
-    "no-restricted-imports": [
-      "error",
+  'rules': {
+    'no-restricted-imports': [
+      'error',
       {
-        "patterns": ["@mui/*/*/*", "!@mui/material/test-utils/*"]
+        'patterns': ['@mui/*/*/*', '!@mui/material/test-utils/*']
       }
     ],
-    "class-methods-use-this": "off",
-    "no-restricted-syntax": "off",
-    "no-continue": "off",
-    "object-curly-newline": "off",
-    "arrow-parens": "off",
-    "no-console": "off",
-    "max-len": "off",
-    "implicit-arrow-linebreak": "off",
-    "operator-linebreak": "off",
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-    "function-paren-newline": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/indent": "off",
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-    "import/extensions": "off"
+    'class-methods-use-this': 'off',
+    'no-restricted-syntax': 'off',
+    'no-continue': 'off',
+    'object-curly-newline': 'off',
+    'arrow-parens': 'off',
+    'no-console': 'off',
+    'max-len': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'operator-linebreak': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'function-paren-newline': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/indent': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'max-classes-per-file': 'off',
+    'no-await-in-loop': 0,
+    'import/no-relative-packages': 'off',
+    '@typescript-eslint/naming-convention': 'off'
   }
-	env: {
-		'jest/globals': true,
-	},
 };
 
 module.exports = eslint;
